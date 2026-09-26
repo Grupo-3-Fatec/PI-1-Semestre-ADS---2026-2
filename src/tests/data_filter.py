9f2f44d3a998d53b64b8155ad79f23129def723b
Lucas <ferlucas150703@gmail.com>
2026-09-25 21:51:34 -0300

    Função finalizada

--- src/tests/data_filter.py
+++ src/tests/data_filter.py
@@ -3,35 +3,16 @@ import pandas as pd
 #importando tipagem generica
 import typing as Any
 
-#definição do DataFrame com base no arquivo csv
-df = pd.read_csv('../../data/data.csv', delimiter=";")
-
 #criando a função generica que aceita collumn apenas como string,
 #value como qualquer valor e retorna um DataFrame
-def singleDataFilter(collumn: str, value: Any) -> pd.DataFrame:
+def singleDataFilter(data_frame: pd.DataFrame, collumn: str, value: Any) -> pd.DataFrame:
 
     #filtra as linhas que batem com a requisição.
     #dentro de "filter" vai conter apenas as linhas em boolean
-    filter = df[collumn] == value
+    filter = data_frame[collumn] == value
 
     #pega as linhas que estao como True e devolve os valores
-    response = df[filter]
+    response = data_frame[filter]
 
     return response
 
-multipleDataFilter()
-
-
-
-
-
-
-
-#teste de fato. nao sera considerado no codigo final!!!
-print('Vamos buscar dados na tabela!')
-collumn = input('Digite o nome da coluna: ')
-value = input('Digite o valor a ser filtrado: ')
-
-response = singleDataFilter(collumn, value)
-
-response.to_csv('resultado.csv')
